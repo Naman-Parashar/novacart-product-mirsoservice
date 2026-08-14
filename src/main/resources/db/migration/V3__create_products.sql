f19e0879e9b33e4356e73cd3b037e71b91d13409
namanparashar <namanparashar484@gmail.com>
2026-08-14 23:51:07 +0530

    Int to BigInt

--- src/main/resources/db/migration/V3__create_products.sql
+++ src/main/resources/db/migration/V3__create_products.sql
@@ -1,5 +1,5 @@
 create table products (
-                          id int not null AUTO_INCREMENT primary key,
+                          id BIGINT not null AUTO_INCREMENT primary key,
                           product_name varchar(255) not null,
                           product_image_url varchar(255) not null default 'nothing.jpg',
                           product_brand varchar(255) not null default 'N/A',
@@ -7,12 +7,12 @@ create table products (
                           product_description text ,
                           price decimal(10,2) not null default 0,
                           stock_quantity int not null default 0,
-                          created_at datetime default current_timestamp,
-                          updated_at datetime default current_timestamp on update current_timestamp,
-                          deleted_at datetime default NULL,
-                          is_active boolean not null default true,
-                          created_by int not null default 1111,
-                          updated_by int not null default 1111,
-                          category_id int not null,
+                          created_at TIMESTAMP default current_timestamp,
+                          updated_at TIMESTAMP default current_timestamp on update current_timestamp,
+                          deleted_at TIMESTAMP default NULL,
+                          is_active BOOLEAN not null default false,
+                          created_by BIGINT not null default 0000,
+                          updated_by BIGINT default 0000,
+                          category_id BIGINT not null,
                           foreign key (category_id) references categories(id)
 );
