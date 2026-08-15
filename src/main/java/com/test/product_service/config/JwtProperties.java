26bdd712ec98a61d2da3f8204bb14f85be0f60a9
namanparashar <namanparashar484@gmail.com>
2026-08-16 01:31:59 +0530

    application Startup will fail if JWT_SECRET is missing from env

--- src/main/java/com/test/product_service/config/JwtProperties.java
+++ src/main/java/com/test/product_service/config/JwtProperties.java
@@ -1,10 +1,15 @@
 package com.test.product_service.config;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @ConfigurationProperties(prefix = "app.jwt")
+@Validated
 public class JwtProperties {
+
+    @NotBlank(message = "JWT secret must be configured")
     private String secret;
 }
